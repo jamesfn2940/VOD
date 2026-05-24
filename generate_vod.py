f4de0c40448ad57ef4c7e7b09c9f7b7d880e9905
jamesfn2940 <jameshikute@gmail.com>
2026-05-24 21:57:13 +0700

    Update generate_vod.py

--- generate_vod.py
+++ generate_vod.py
@@ -146,7 +146,7 @@ def list_recursive(folder_id, group_name, token):
 
 
 def make_stream_url(file_id):
-    return f"https://drive.google.com/uc?export=download&id={file_id}"
+    return f"https://drive.google.com/file/d/{file_id}/preview"
 
 
 def generate_m3u(files, output_path):
